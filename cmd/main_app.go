9194b67451360a4f6535ffb8dc4495b9a5d5ad46
Kaan Taha Köken <kaan_taha_koken@hotmail.com>
2022-09-01 19:48:43 +0300

    Add mainapp added for testing & maintaning DI

---
+++ cmd/main_app.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/kaankoken/helper/pkg/helper"
+	"github.com/kaankoken/versioning-tool/pkg"
+	versionlabel "github.com/kaankoken/versioning-tool/pkg/version-label"
+	"go.uber.org/fx"
+)
+
+// MainApp -> Registering main app to FX
+func MainApp(args []string) (app *fx.App, err error) {
+	// argument controls
+	if len(args) <= 0 {
+		return nil, fmt.Errorf("%s", "Wrong number of inputs, no arguments found")
+	}
+
+	if len(args) < 3 {
+		return nil, fmt.Errorf("%s", "Wrong number of inputs, cannot be less than 3")
+	}
+
+	input := pkg.Run(args[0], args[1], args[2], args[3:]...)
+
+	a := fx.New(
+		fx.Supply(input),
+		pkg.TagModule,
+		pkg.FlavorModule,
+		helper.LoggerModule,
+		fx.Invoke(RegisterHooks),
+		versionlabel.VersionLabelModule,
+	)
+
+	return a, nil
+}
+
+// RegisterHooks -> Registering lifecycle of fx & running http server (Gin)
+func RegisterHooks(lifecycle fx.Lifecycle, logger *helper.LogHandler) {
+	lifecycle.Append(
+		fx.Hook{
+			OnStart: func(context.Context) error {
+				logger.Info("Starting application")
+				return nil
+			},
+			OnStop: func(context.Context) error {
+				logger.Info("Stopping application")
+				return nil
+			},
+		},
+	)
+}
